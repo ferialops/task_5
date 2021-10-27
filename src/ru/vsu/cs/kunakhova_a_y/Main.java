0e5472e535e9147892f80af62556411cb8ffaf6c
ferialops <90615135+ferialops@users.noreply.github.com>
2021-10-27 14:15:17 +0300

    Add files via upload

---
+++ src/ru/vsu/cs/kunakhova_a_y/Main.java
@@ -0,0 +1,67 @@
+package u.vsu.cs.kunakhova_a_y;
+
+import java.util.Scanner;
+
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.ROOT);
+
+        figure(readW());
+
+    }
+
+    public static int readW() {
+        System.out.print("Enter width w = ");
+        Scanner sc = new Scanner(System.in);
+        int w = sc.nextInt();
+        if (w <= 2) {
+            System.out.println("Width can be only more than 2");
+            System.exit(1);
+        }
+
+        return w;
+    }
+
+    public static void newString(int w, int spaces, int exclamationMarks) {
+        System.out.print("|");
+        for (int i = 0; i < spaces; i++) {
+            System.out.print(" ");
+        }
+        for (int i = 0; i < exclamationMarks; i++) {
+            if ((i + spaces) > (w - 2)) break;
+            System.out.print("!");
+        }
+        for (int i = 0; i < (w - spaces - exclamationMarks - 2); i++) {
+            System.out.print(" ");
+        }
+        System.out.println("|");
+    }
+
+    public static void firstAndSecondString(int w) {
+        System.out.print(" ");
+        for (int i = 0; i < (w - 2); i++) {
+            System.out.print("-");
+        }
+        System.out.println(" ");
+    }
+
+    public static void figure(int w) {
+        int spaces = 0;
+        int exclamationMarks;
+        firstAndSecondString(w);
+        for (exclamationMarks = 1; exclamationMarks < (w - 2 - spaces); exclamationMarks++) {
+            newString(w, spaces, exclamationMarks);
+            spaces += exclamationMarks;
+        }
+        newString(w, spaces, (w - spaces - 2));
+
+        while (exclamationMarks > 1) {
+            exclamationMarks -= 1;
+            spaces -= exclamationMarks;
+            newString(w, spaces, exclamationMarks);
+        }
+        firstAndSecondString(w);
+    }
+}
